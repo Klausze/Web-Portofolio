b20d798d2b91e5c6839077e9efb15c64d6a99b30
Raditya Ananta Maheswara <maheswararaditya07@gmail.com>
2026-04-02 10:34:25 +0700

    upload pertama

---
+++ script.js
@@ -0,0 +1,9 @@
+// ---- BAGIAN UNTUK INDEX.HTML ----
+const btn = document.getElementById("helloBtn");
+const msg = document.getElementById("message");
+
+if (btn && msg) {
+    btn.addEventListener("click", () => {
+        msg.textContent = "Halo! Kamu baru saja menekan tombol 🎉";
+    });
+}
